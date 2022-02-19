9df2fbb8bdde8193cde1c4decef4ba80a3725b7a
Ola-22 <olanazly@gmail.com>
2022-02-19 20:25:02 +0200

    Edit pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Header from "./Components/Header";
@@ -17,7 +17,7 @@ function App() {
         category && `&category=${category}`
       }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
     );
-    setQuestions(data.results.question);
+    setQuestions(data.results);
     console.log(data);
   };
   return (
@@ -25,7 +25,18 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/quiz" element={<Quiz />} />
+          <Route
+            path="/quiz"
+            element={
+              <Quiz
+                name={name}
+                questions={questions}
+                score={score}
+                setScore={setScore}
+                setQuestions={setQuestions}
+              />
+            }
+          />
           <Route path="/result" element={<Result />} />
           <Route
             path="/"
